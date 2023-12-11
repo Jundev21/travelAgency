7eeb97ba1b8f1a933d77e24fe5634a3f8e9a152f
jun <jundev21@gmail.com>
2023-12-12 01:49:20 +0900

    [feat] 객실 조회 API 추가

--- src/main/java/com/travelAgency/travelAgency/domain/room/controller/RoomController.java
+++ src/main/java/com/travelAgency/travelAgency/domain/room/controller/RoomController.java
@@ -1,4 +1,27 @@
 package com.travelAgency.travelAgency.domain.room.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.travelAgency.travelAgency.domain.room.dto.response.RoomResponseDto;
+import com.travelAgency.travelAgency.domain.room.service.RoomService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
 public class RoomController {
+
+	private final RoomService roomService;
+
+	@GetMapping("/hotel/{hotelId}/room")
+	public ResponseEntity<List<RoomResponseDto>> roomDetails(
+		@PathVariable long hotelId
+	){
+		return roomService.roomDetails(hotelId);
+	}
 }
--- src/main/java/com/travelAgency/travelAgency/domain/room/service/RoomService.java
+++ src/main/java/com/travelAgency/travelAgency/domain/room/service/RoomService.java
@@ -1,10 +1,20 @@
 package com.travelAgency.travelAgency.domain.room.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.deser.BasicDeserializerFactory;
+import com.travelAgency.travelAgency.domain.common.ErrorCode;
+import com.travelAgency.travelAgency.domain.common.NormalException;
+import com.travelAgency.travelAgency.domain.hotel.entity.Hotels;
+import com.travelAgency.travelAgency.domain.hotel.repository.HotelRepository;
+import com.travelAgency.travelAgency.domain.hotel.service.HotelService;
+import com.travelAgency.travelAgency.domain.room.dto.RoomMapper;
+import com.travelAgency.travelAgency.domain.room.dto.response.RoomResponseDto;
 import com.travelAgency.travelAgency.domain.room.entity.Rooms;
 import com.travelAgency.travelAgency.domain.room.repository.RoomRepository;
 
@@ -17,7 +27,14 @@ import lombok.RequiredArgsConstructor;
 public class RoomService {
 
 	private final RoomRepository roomRepository;
+	private final HotelService hotelService;
 
+	public ResponseEntity<List<RoomResponseDto>> roomDetails(long hotelId) {
+		Hotels hotels = hotelService.getHotel(hotelId);
+		return ResponseEntity.ok(hotels.getRoomsList().stream()
+				.map(RoomMapper.INSTANCE::roomResponseDto)
+				.toList());
+	}
 
 	public String findLowestPrice(Long hotelId){
 		// 룸 최저가가 각 호텔에 맞는 최저가를 리턴해준다.
