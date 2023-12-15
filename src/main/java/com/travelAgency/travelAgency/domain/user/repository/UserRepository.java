61bf73d34c9adf751289ca135a7061d7a7438e4c
jun <jundev21@gmail.com>
2023-12-16 01:05:52 +0900

    [updqte] 회원 중복 가입여부 체크

--- src/main/java/com/travelAgency/travelAgency/domain/user/repository/UserRepository.java
+++ src/main/java/com/travelAgency/travelAgency/domain/user/repository/UserRepository.java
@@ -11,4 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<Users,Long> {
 
 	Optional<Users> findByEmail(String userEmail);
+
+	Boolean existsByEmail(String userEmail);
 }
