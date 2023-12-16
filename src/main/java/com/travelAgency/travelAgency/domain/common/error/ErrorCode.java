b58268775542626025b1f69c4438f10a0992eb44
jun <jundev21@gmail.com>
2023-12-17 02:12:09 +0900

    [update] 에러코드 추가

--- src/main/java/com/travelAgency/travelAgency/domain/common/error/ErrorCode.java
+++ src/main/java/com/travelAgency/travelAgency/domain/common/error/ErrorCode.java
@@ -8,6 +8,10 @@ import lombok.Getter;
 public enum ErrorCode implements ErrorCodeInterface {
 	NO_HOTEL_ID("해당 호텔이 없습니다"),
 	NO_ROOM_ID("해당 객실이 없습니다"),
-	NO_USER_EMAIL("해당 사용자가 없습니다");
+	NO_USER_EMAIL("해당 사용자가 없습니다"),
+	NO_REVIEW_ID("해당 리뷰가 없습니다."),
+	ALREADY_REGISTER_USER("이미 등록한 회원입니다."),
+	NO_ROOMS_STOCKS("해당 객실의 재고가 없습니다.")
+	;
 	private final String errorMessage;
-}
+	}
